88b8ec0af1134e489d6479d0f5641f258c2f5343
Alan Pisani <alanpisani1993@gmail.com>
2023-05-31 15:11:59 -0300

    Agregado html ticket, con funcionalidad del resumen

---
+++ script.js
@@ -0,0 +1,36 @@
+const cantidad = document.querySelector('#cantidad');
+const categoria = document.querySelector('#categoria');
+const resumen = document.querySelector('#resumen');
+const btnBorrar = document.querySelector('#btn-borrar');
+const btnResumen = document.querySelector('#btn-resumen');
+
+
+
+btnResumen.addEventListener("click", ()=>{
+    let indice = categoria.selectedIndex;
+    let valor = cantidad.value;
+    let descuento = 0
+    let total = 0
+    //console.log(indice)
+
+    if(valor == ""){
+        valor = 0
+    }
+
+    if(indice == 0){
+
+        descuento = (80*valor) / 100
+        
+    }else if(indice == 1){
+
+        descuento = (50*valor) / 100
+
+    }else{
+
+        descuento = (15*valor) / 100
+    }
+    total = valor - descuento
+    resumen.innerHTML = total;
+})
+
+
